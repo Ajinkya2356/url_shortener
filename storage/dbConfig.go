578d7ab59d8ad9e26d167621c95cd0b9b3548441
Ajinkya Jagtap <ajinkyajagtap2806@gmail.com>
2025-01-17 14:30:24 +0530

    commit

--- storage/dbConfig.go
+++ storage/dbConfig.go
@@ -1,34 +1,36 @@
 package storage
 
 import (
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"log"
-	"os"
+    "github.com/joho/godotenv"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "log"
+    "os"
 )
 
 type URL struct {
-	gorm.Model
-	OriginalURL string `gorm:"unique"`
-	ShortURL    string `gorm:"unique"`
+    gorm.Model
+    OriginalURL string `gorm:"unique"`
+    ShortURL    string `gorm:"unique"`
 }
 
 func InitDB() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	dsn := "host=" + os.Getenv("DB_HOST") +
-		" user=" + os.Getenv("DB_USER") +
-		" password=" + os.Getenv("DB_PASSWORD") +
-		" dbname=" + os.Getenv("DB_NAME") +
-		" port=" + os.Getenv("DB_PORT") +
-		" sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database", err)
-	}
-	db.AutoMigrate(&URL{})
-	return db
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Use the DATABASE_URL environment variable
+    databaseURL := os.Getenv("DATABASE_URL")
+    if databaseURL == "" {
+        log.Fatal("DATABASE_URL environment variable is not set")
+    }
+
+    db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to database", err)
+    }
+
+    db.AutoMigrate(&URL{})
+    return db
 }
